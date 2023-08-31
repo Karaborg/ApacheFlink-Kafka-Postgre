a156c1fdc73f359892d08bcaaf530c5ff1fb453a
furkan <furkancrt96@gmail.com>
2023-08-31 18:14:06 +0300

    Update kafkapushmessage.py

--- kafkapushmessage.py
+++ kafkapushmessage.py
@@ -1,8 +1,1 @@
-python3 main.py \
-  --security-protocol plaintext \
-  --host localhost \
-  --port 9092 \
-  --topic-name pizza-orders \
-  --nr-messages 0 \
-  --max-waiting-time 0 \
-  --subject pizza
+python3 main.py --security-protocol plaintext --host localhost --port 9092 --topic-name pizza-orders --nr-messages 0 --max-waiting-time 0 --subject pizza
